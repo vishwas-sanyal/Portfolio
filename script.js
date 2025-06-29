bd7f94576b081e9481204e4f7acc67612ef188c0
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-06-29 18:13:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let links1 = document.querySelector("#tictactoe");
+links1.addEventListener("dblclick", () => {
+    window.open("https://github.com/vishwas-sanyal/tic-tac-toe");
+});
+
+let links2 = document.querySelector("#portfolio");
+links2.addEventListener("dblclick", () => {
+    window.open("https://github.com/vishwas-sanyal/Portfolio");
+});
+
+let links3 = document.querySelector("#wd");
+links3.addEventListener("dblclick", () => {
+    window.open("https://github.com/vishwas-sanyal/Web-Drums");
+});
+
+let links4 = document.querySelector("#asl");
+links4.addEventListener("dblclick", () => {
+    window.open("https://github.com/vishwas-sanyal/ASL");
+});
+
+let links5 = document.querySelector("#vi");
+links5.addEventListener("dblclick", () => {
+    window.open("https://github.com/vishwas-sanyal/Virtual-Instrument");
+});
+
+let links6 = document.querySelector("#ai");
+links6.addEventListener("dblclick", () => {
+    window.open("https://www.linkedin.com/posts/vishwas-sanyal-b6b880291_virtualassistant-productivity-innovation-activity-7194955781312581632-RPl0");
+});
